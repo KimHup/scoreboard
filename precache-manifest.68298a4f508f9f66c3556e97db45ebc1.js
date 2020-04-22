7c5319d5cf2c6094f76cd3ca8375e58e2cb7c549
Kim <kim.hupponen@abo.fi>
2020-04-22 15:55:24 +0300

    Updates

---
+++ precache-manifest.68298a4f508f9f66c3556e97db45ebc1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d51b6b765023c0f6b6b4c8aa683b0880",
+    "url": "/scoreboard/index.html"
+  },
+  {
+    "revision": "cf3a07ebb14531532207",
+    "url": "/scoreboard/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0aa139c49476ef949b56",
+    "url": "/scoreboard/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf3a07ebb14531532207",
+    "url": "/scoreboard/static/js/2.e8b4a582.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/scoreboard/static/js/2.e8b4a582.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0aa139c49476ef949b56",
+    "url": "/scoreboard/static/js/main.b4e488d6.chunk.js"
+  },
+  {
+    "revision": "a2fb0d685d7d149df85e",
+    "url": "/scoreboard/static/js/runtime-main.567d7b41.js"
+  }
+]);
